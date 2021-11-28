2bf9200efb774536423c0bb5dbdccdc12432fa42
lixinyang123 <lixinyangemil@outlook.com>
2021-11-28 18:01:32 +0800

    add docker file

--- build.ps1
+++ build.ps1
@@ -1,1 +1,11 @@
+echo "======================"
+echo "| Build Docker Image |"
+echo "======================"
+
+docker build -t lixinyang/cloudshell:clouddt ./CloudDT.Shared
+
+echo "========================"
+echo "| Build Desktop Client |"
+echo "========================"
+
 dotnet publish .\CloudDT.Shared\CloudDT.Shared.csproj -o ./CloudDT.Client/publish
